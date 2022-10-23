615b8fc217452fb93b5d7dc91b6e56de6cc5f290
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-10-23 11:19:13 +0200

    Update database_serv.R

--- source/server/database_serv.R
+++ source/server/database_serv.R
@@ -3,14 +3,10 @@
 selected_db <- reactiveVal()
 observeEvent(input$select_operation, {
 
-  selected <- react_db() %>%
-    #select_by(by = "type", value = c(find_types,
-    #                                 "Pottery_Quantification_A",
-    #                                 "Pottery_Quantification_B")) %>%
-    # also prep it to be better readable
-    prep_for_shiny(reorder_periods = reorder_periods) %>%
-    filter(id %in% uid_by_operation(filter_operation = input$select_operation,
-                                    index = react_index()))
+  selected <- react_db() #%>%
+    #prep_for_shiny(reorder_periods = reorder_periods) %>%
+    #filter(id %in% uid_by_operation(filter_operation = input$select_operation,
+    #                                index = react_index()))
 
   selected_db(selected)
   rm(selected)
