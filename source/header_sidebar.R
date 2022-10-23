a2f21e4c295c7edfb8c1b748d152ae73f3735c45
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2022-10-23 18:01:35 +0200

    Update header_sidebar.R

--- source/header_sidebar.R
+++ source/header_sidebar.R
@@ -7,15 +7,19 @@ header <- dashboardHeader(title = "milQuant",
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Connect", tabName = "connect", icon = icon("sign-in-alt")),
+    menuItem("Connect", tabName = "connect", icon = icon("right-to-bracket")),
     uiOutput("select_operation"),
     uiOutput("select_trench"),
     menuItem("Home (Project)", tabName = "home", icon = icon("graduation-cap")),
     menuItem("Finds (Overview)", tabName = "allfinds", icon = icon("chart-bar")),
     menuItem("Pottery", tabName = "pottery_all", icon = icon("trophy"),
-             menuSubItem("Pottery (single)", tabName = "pottery", icon = icon("wine-glass-alt")),
-             menuSubItem("Pottery Quantification A", tabName = "potteryQA", icon = icon("glass-cheers")),
-             menuSubItem("Pottery Quantification B", tabName = "potteryQB", icon = icon("glass-cheers"))
+          menuSubItem("Pottery (single)", tabName = "pottery",
+                      icon = icon("martini-glass-empty")),
+          menuSubItem("Pottery Quantification A", tabName = "potteryQA",
+                      icon = icon("champagne-glasses")),
+          menuSubItem("Pottery Quantification B", tabName = "potteryQB",
+                      icon = icon("champagne-glasses"))
+    ),
     menuItem("Bricks and Tiles", tabName = "bricks_all", icon = icon("square"),
           menuSubItem("Bricks and Tiles", tabName = "bricks",
                       icon = icon("house")),
