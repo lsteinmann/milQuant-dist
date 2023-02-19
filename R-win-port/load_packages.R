2daf31f1849ebf54e5b1319146e762b188bbcf37
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-02-19 22:43:22 +0100

    Update load_packages.R

--- R-win-port/load_packages.R
+++ R-win-port/load_packages.R
@@ -1,7 +1,13 @@
-packages <- c("shiny", "shinyWidgets", "glue")
-
+packages <- c("shiny", "shinydashboard",
+              "shinycssloaders", "shinyjs", "shinyWidgets",
+              "ggplot2", "dplyr", "reshape2", "viridis", "forcats", "tidyr",
+              "remotes", "idaifieldR", "sofa",
+              "glue")
 for (p in packages) {
-  install.packages(p, lib = "library/")#, dependencies = TRUE)
-  # or try something with automagic?
+    if (p == "idaifieldR") {
+      remotes::install_github("lsteinmann/idaifieldR@v0.2.2", lib = "library/", force = TRUE)
+    } else {
+      install.packages(p, lib = "library/")
+    }
 }
 rm(packages, p)
