5041a72624f47595a7bfb73b118dd62230843d17
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-02-20 08:53:32 +0100

    Update app.R

--- app.R
+++ app.R
@@ -33,7 +33,7 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
    
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
@@ -43,6 +43,10 @@ server <- function(input, output) {
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
+
+   session$onSessionEnded(function() {
+      stopApp()
+    })
 }
 
 # Run the application 
