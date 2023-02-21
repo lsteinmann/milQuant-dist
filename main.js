592e21b377703632c33b4f0108152c3160da17af
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-02-21 17:32:47 +0100

    comment
    and a question into the void: why?

--- main.js
+++ main.js
@@ -125,6 +125,12 @@ function cleanUpApplication(){
     childProcess.kill(); 
   }
 }
+
+
+// This app does not need a gpu
+// app.disableHardwareAcceleration()
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
