7963a941c1e20297ea2729b4c5453e6b0e039b01
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-02-24 11:50:17 +0100

    format

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 // not great to do that, maybe try to choose a random port?
@@ -13,50 +13,50 @@ const child = require('child_process');
 // for options see: 
 // https://www.electronforge.io/config/makers/squirrel.windows
 // https://js.electronforge.io/interfaces/_electron_forge_maker_squirrel.InternalOptions.Options.html
-var handleSquirrelEvent = function() {
+var handleSquirrelEvent = function () {
   if (process.platform != 'win32') {
-     return false;
+    return false;
   }
 
   function executeSquirrelCommand(args, done) {
-     var updateDotExe = path.resolve(path.dirname(process.execPath), 
-        '..', 'update.exe');
-     var sqchild = child.spawn(updateDotExe, args, { detached: true });
+    var updateDotExe = path.resolve(path.dirname(process.execPath),
+      '..', 'update.exe');
+    var sqchild = child.spawn(updateDotExe, args, { detached: true });
 
-     sqchild.on('close', function(code) {
-        done();
-     });
+    sqchild.on('close', function (code) {
+      done();
+    });
   };
 
   function install(done) {
-     var target = path.basename(process.execPath);
-     executeSquirrelCommand(["--createShortcut", target], done);
+    var target = path.basename(process.execPath);
+    executeSquirrelCommand(["--createShortcut", target], done);
   };
 
   function uninstall(done) {
-     var target = path.basename(process.execPath);
-     executeSquirrelCommand(["--removeShortcut", target], done);
+    var target = path.basename(process.execPath);
+    executeSquirrelCommand(["--removeShortcut", target], done);
   };
 
   var squirrelEvent = process.argv[1];
 
   switch (squirrelEvent) {
 
-     case '--squirrel-install':
-        install(app.quit);
-        return true;
+    case '--squirrel-install':
+      install(app.quit);
+      return true;
 
-     case '--squirrel-updated':
-        install(app.quit);
-        return true;
+    case '--squirrel-updated':
+      install(app.quit);
+      return true;
 
-     case '--squirrel-obsolete':
-        app.quit();
-        return true;
+    case '--squirrel-obsolete':
+      app.quit();
+      return true;
 
-     case '--squirrel-uninstall':
-        uninstall(app.quit);
-        return true;
+    case '--squirrel-uninstall':
+      uninstall(app.quit);
+      return true;
   }
 
   return false;
@@ -71,13 +71,13 @@ if (handleSquirrelEvent()) {
 // (remodel) change forward slashes to escaped backslashes to hand the path to R/shiny
 // anything else but windows is not supported here at all and this part has to be redone
 // if you want to distribute to something other than windows
-var appPath = path.join(app.getAppPath(), "shiny/app.R" ).replace(/\\/g, "\\\\")
-var execPath = path.join(app.getAppPath(), "R-win-port", "bin", "RScript.exe" )
+var appPath = path.join(app.getAppPath(), "shiny/app.R").replace(/\\/g, "\\\\")
+var execPath = path.join(app.getAppPath(), "R-win-port", "bin", "RScript.exe")
 
 
 // creates the childProcess const that will start R and tell it to run the Shiny App as app.R from the 
 // app directory of the electron app
-const childProcess = child.spawn(execPath, ["-e", "shiny::runApp(file.path('"+appPath+"'), port="+port+")"])
+const childProcess = child.spawn(execPath, ["-e", "shiny::runApp(file.path('" + appPath + "'), port=" + port + ")"])
 
 // this starts the childProcess and also
 // repeats everything R tells us to the console
@@ -91,7 +91,7 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 const delayedLoad = async () => {
   mainWindow.loadFile('loading.html')
   await delay(3000);
-  mainWindow.loadURL('http://127.0.0.1:'+port)
+  mainWindow.loadURL('http://127.0.0.1:' + port)
 };
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -100,7 +100,7 @@ let mainWindow
 
 
 // Create the browser window.
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   console.log('Creating the main window here')
 
@@ -110,7 +110,7 @@ function createWindow () {
     frame: true,
     show: false,
     webPreferences: {
-      nodeIntegration:false
+      nodeIntegration: false
     }
   })
 
@@ -121,11 +121,11 @@ function createWindow () {
 }
 
 // quit the app, and if a child exists, kill it
-function cleanUpApplication(){
+function cleanUpApplication() {
   app.quit()
-  
-  if(childProcess){
-    childProcess.kill(); 
+
+  if (childProcess) {
+    childProcess.kill();
   }
 }
 
