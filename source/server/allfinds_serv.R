2db064b80eabdd35201dd19f1e7fb7bcd4833a83
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-04 17:10:54 +0100

    add workflow tabbox to allfinds plot

--- source/server/allfinds_serv.R
+++ source/server/allfinds_serv.R
@@ -66,29 +66,32 @@ allFindsPlot_data <- reactive({
   validate(
     need(is.character(input$findPlot_PlotVar), "No variable selected.")
   )
-  data <- findPlot_base_data() %>%
+  allFindsPlot_data <- findPlot_base_data() %>%
     filter(relation.liesWithinLayer %in% input$findPlot_layer_selector) %>%
-    period_filter(is_milet = is_milet, selector = input$findPlot_period_selector) %>%
+    period_filter(is_milet = is_milet, selector = input$findPlot_period_selector)
+
+  return(allFindsPlot_data)
+})
+
+make_allFindsPlot <- reactive({
+  plot_data <- allFindsPlot_data() %>%
     mutate(var = get(input$findPlot_PlotVar)) %>%
     select(type, var) %>%
     mutate(type = fct_infreq(type))
 
-  if (is.logical(data$var)) {
-    data <- data %>%
+  if (is.logical(plot_data$var)) {
+    plot_data <- plot_data %>%
       mutate(var = ifelse(is.na(var),
                           FALSE,
                           TRUE))
   } else {
-    data <- data %>%
+    plot_data <- plot_data %>%
       mutate(var = fct_infreq(var))
   }
 
-  return(data)
-})
 
-make_allFindsPlot <- reactive({
   if (input$findPlot_axis == "var_is_fill") {
-    p <- allFindsPlot_data() %>%
+    p <- plot_data %>%
       ggplot(aes(x = type,
                  fill = var)) +
       labs(fill = input$findPlot_PlotVar,
@@ -99,12 +102,12 @@ make_allFindsPlot <- reactive({
     }
   } else if (input$findPlot_axis == "var_is_x") {
     if (input$findPlot_PlotVar == "date") {
-      p <- allFindsPlot_data() %>%
+      p <- plot_data %>%
         ggplot(aes(x = date,
                    fill = type)) +
         scale_x_date(name = "Date of Processing")
     } else {
-      p <- allFindsPlot_data() %>%
+      p <- plot_data %>%
         ggplot(aes(x = var,
                    fill = type)) +
         labs(fill = "Type of Find",
@@ -117,7 +120,7 @@ make_allFindsPlot <- reactive({
     labs(title = input$findPlot_title,
          subtitle = input$findPlot_subtitle,
          caption = paste("Total number of objects: ",
-                         nrow(allFindsPlot_data()), sep = ""))
+                         nrow(plot_data), sep = ""))
   p
 })
 
@@ -126,11 +129,70 @@ output$allFindsPlot <- renderPlotly({
 })
 
 
-
-
-
-
 output$allFindsPlot_png <- milQuant_dowloadHandler(plot = make_allFindsPlot(),
                                                 ftype = "png")
 output$allFindsPlot_pdf <- milQuant_dowloadHandler(plot = make_allFindsPlot(),
                                                 ftype = "pdf")
+
+
+## Workflow Tabbox
+
+output$allfinds_workflow <- renderUI({
+
+#  workflow <- allFindsPlot_data() %>%
+#    select(identifier, contains("workflow"))
+
+  workflow_cols <- as.list(grep("workflow", colnames(allFindsPlot_data())))
+  workflow_cols <- colnames(allFindsPlot_data())[unlist(workflow_cols)]
+
+  total <- nrow(allFindsPlot_data())
+
+
+
+  do.call(tabBox,
+          append(list(width = 12, title = tagList(icon("gear"), "Workflow status")),
+                 lapply(workflow_cols,
+                        function(wfcol) {
+                          # get the columns index
+                          id <- which(colnames(allFindsPlot_data()) == wfcol)
+                          # get all identifiers that apply
+                          res <- which(allFindsPlot_data()[, id] == TRUE)
+                          res <- as.character(allFindsPlot_data()$identifier[res])
+                          res <- sort(res)
+                          # get all identifiers that don't apply
+                          neg <- which(is.na(allFindsPlot_data()[, id]))
+                          neg <- as.character(allFindsPlot_data()$identifier[neg])
+                          neg <- sort(neg)
+
+                          title <- gsub("workflow.", "", wfcol, fixed = TRUE)
+                          # display in green if its a good thing, in red if
+                          # "fehlerhaft"
+                          res_style <- ifelse(grepl("Fehlerhaft", wfcol),
+                                              "color:red",
+                                              "color:green")
+                          perc <- round(length(res) / total * 100, digits = 1)
+                          col <- ifelse(grepl("Fehlerhaft", wfcol),
+                                        "red", "blue")
+
+                          tabPanel(title = title,
+                                   fluidRow(
+                                     infoBox(width = 8, color = col,
+                                             title = title,
+                                             value = p("Applies to", strong(length(res)),
+                                                       "out of", strong(total),
+                                                       "objects (",
+                                                       perc, "%).")),
+                                     valueBox(subtitle = "Progress",
+                                       value = paste0(perc, "%"),
+                                       icon = icon("list"), width = 4,
+                                       color = col)),
+                                   h3("Objects in the plot where this box has been checked: "),
+                                   p(paste(res, collapse = ", "),
+                                     style = res_style),
+                                   h3("... and objects where it has not:"),
+                                   p(paste(neg, collapse = ", "),
+                                     style = "color:grey"))
+                        })
+          )
+  )
+})
