923e1e8829f802472a648921424198e500fc981d
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-11-13 11:37:19 +0100

    deactivate pnd/pdf download

--- source/global_modules/downloadModule.R
+++ source/global_modules/downloadModule.R
@@ -3,8 +3,8 @@ downloadPlotButtons <- function(id) {
   ns <- NS(id)
 
   div(
-    downloadButton(ns("dPNG"), "Download plot (PNG)"),
-    downloadButton(ns("dPDF"), "Download plot (PDF)"),
+    #downloadButton(ns("dPNG"), "Download plot (PNG)"),
+    #downloadButton(ns("dPDF"), "Download plot (PDF)"),
     downloadButton(ns("dHTML"), "Download plot (HTML)")
   )
 }
@@ -15,25 +15,25 @@ makeDownloadPlotHandler <- function(id, dlPlot) {
     id,
     function(input, output, session) {
 
-      output$dPNG <- downloadHandler(
-        filename = paste(format(Sys.Date(), "%Y%m%d"),
-                         "_milQuant_plot.png", sep = ""),
-        content <- function(file) {
-          ggsave(file, plot = dlPlot() + Plot_Base_Theme + Plot_Base_Guide,
-                 device = "png",
-                 width = 25, height = 15, units = "cm")
-        }
-      )
+      #output$dPNG <- downloadHandler(
+      #  filename = paste(format(Sys.Date(), "%Y%m%d"),
+      #                   "_milQuant_plot.png", sep = ""),
+      #  content <- function(file) {
+      #    ggsave(file, plot = dlPlot() + Plot_Base_Theme + Plot_Base_Guide,
+      #           device = "png",
+      #           width = 25, height = 15, units = "cm")
+      #  }
+      #)
 
-      output$dPDF <- downloadHandler(
-        filename = paste(format(Sys.Date(), "%Y%m%d"),
-                         "_milQuant_plot.pdf", sep = ""),
-        content <- function(file) {
-          ggsave(file, plot = dlPlot() + Plot_Base_Theme + Plot_Base_Guide,
-                 device = "pdf",
-                 width = 25, height = 15, units = "cm")
-        }
-      )
+      #output$dPDF <- downloadHandler(
+      #  filename = paste(format(Sys.Date(), "%Y%m%d"),
+      #                   "_milQuant_plot.pdf", sep = ""),
+      #  content <- function(file) {
+      #    ggsave(file, plot = dlPlot() + Plot_Base_Theme + Plot_Base_Guide,
+      #           device = "pdf",
+      #           width = 25, height = 15, units = "cm")
+      #  }
+      #)
 
       output$dHTML <- downloadHandler(
         filename = paste(format(Sys.Date(), "%Y%m%d"),
